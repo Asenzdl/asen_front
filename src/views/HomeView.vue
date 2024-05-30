aae8cfe52afa591dd8d2279747e685fa06cd4c48
Asenzdl <414967707@qq.com>
2024-05-30 21:39:27 +0800

    初始提交

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+function a() {
+  console.log('Hello World')
+}
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
